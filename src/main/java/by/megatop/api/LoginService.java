82868a58b134e3a079eb1099a10b983459443534
n.khadasevich <nataliabizunkova@gmail.com>
2024-03-29 13:37:12 +0300

    add api login tests

--- src/main/java/by/megatop/api/LoginService.java
+++ src/main/java/by/megatop/api/LoginService.java
@@ -1,6 +1,11 @@
 package by.megatop.api;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LoginService {
+
+    public static final String LOGIN_URL="https://megatop.by/api/v1/user/login";
 
     public static String getBody(String email, String password) {
         return String.format("{\n" +
@@ -14,5 +19,11 @@ public class LoginService {
                 "\"email\": \"%s\",\n" +
                 "\"password\": %n\n" +
                 "}", email, password);
+    }
+
+    public static Map<String, String> getHeaders() {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        return headers;
     }
 }
