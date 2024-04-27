d5dc459497ee3f6a3cfdc53e30c9f4c8d445c509
n.khadasevich <nataliabizunkova@gmail.com>
2024-04-27 18:36:37 +0300

    add final settings

--- src/main/java/by/megatop/util/WaiterHelper.java
+++ src/main/java/by/megatop/util/WaiterHelper.java
@@ -17,7 +17,7 @@ public class WaiterHelper {
 
     public static void waitAwaitilityUntilElementAppears(int minNumberOfElements, String locator){
         Awaitility.await()
-                .atMost(60, SECONDS)
+                .atMost(120, SECONDS)
                 .pollInterval(100, MILLISECONDS)
                 .until(() -> {
                     return Driver.getDriver().findElements(By.xpath(locator)).size() > minNumberOfElements;
