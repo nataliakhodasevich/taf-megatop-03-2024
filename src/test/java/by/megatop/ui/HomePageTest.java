78419ca259c249d268774b24a8b12fb3c77a2c09
n.khadasevich <nataliabizunkova@gmail.com>
2024-04-28 15:46:41 +0300

    add final settings

--- src/test/java/by/megatop/ui/HomePageTest.java
+++ src/test/java/by/megatop/ui/HomePageTest.java
@@ -4,12 +4,17 @@ import by.megatop.pages.HomePage;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class HomePageTest extends BaseTest {
+
+    private static final Logger logger = LogManager.getLogger(HomePageTest.class);
     @Test
     @DisplayName("Check the main page is opened")
     public void isPageOpened() {
         HomePage homePage = new HomePage();
+        logger.info("Это информационное сообщение");
         Assertions.assertEquals("© Megatop.\n" +
                 "Все права защищены. 2023", homePage.getCopyRightText());
     }
